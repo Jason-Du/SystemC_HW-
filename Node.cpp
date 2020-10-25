f3a5a387aff2d9b558825e5ceb6780e796632d68
“Jason-Duh(Home) <dushiun@gmail.com>
2020-10-25 12:58:03 +0800

    list.h update

--- Node.cpp
+++ Node.cpp
@@ -1,34 +1,4 @@
 
-Node::Node(){
-	long* _Node=NULL;
-	//int _Length=0;
-}
-
-Node::Node(unsigned int _length) {
-	//_Length=_length;
-    if (_length == 0)
-        _Node = NULL;
-    else {
-        _Node = new int [_length];
-        assert(_Array != NULL);
-    }
-}
-Node::~Node() {
-	if (_Node!=NULL)
-	delete [] _Node;
-	_Node = NULL;
-}
-long* Node::reCreate(unsigned int _length) {
-	if (_Node!=NULL)
-	{
-		delete [] _Node;
-		_Node = NULL;
-	}
-	_Node = new int [_length];
-	assert(_Array != NULL);
-}
-
-
 int main(int argc, char *argv[]) {
 	/*
     List dlist;       // calls the default constructor
--- Node.h
+++ Node.h
@@ -1,5 +1,4 @@
 //h file
-
 class Node {
 private:
 	long* _Node;
@@ -8,3 +7,31 @@ public:
 	~Node();
     long* reCreate(unsigned int _length);
 };
+Node::Node(){
+	long* _Node=NULL;
+	//int _Length=0;
+}
+
+Node::Node(unsigned int _length) {
+	//_Length=_length;
+    if (_length == 0)
+        _Node = NULL;
+    else {
+        _Node = new int [_length];
+        assert(_Array != NULL);
+    }
+}
+Node::~Node() {
+	if (_Node!=NULL)
+	delete [] _Node;
+	_Node = NULL;
+}
+long* Node::reCreate(unsigned int _length) {
+	if (_Node!=NULL)
+	{
+		delete [] _Node;
+		_Node = NULL;
+	}
+	_Node = new int [_length];
+	assert(_Array != NULL);
+}
